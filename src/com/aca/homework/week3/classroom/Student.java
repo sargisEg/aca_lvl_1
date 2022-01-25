aea25512eb25d9b4867d2a5c1da0dc0eec01ff8f
sargisEg <sargis.eg@gmail.com>
2022-01-25 22:14:08 +0400

    Covidtest

---
+++ src/com/aca/homework/week3/classroom/Student.java
@@ -0,0 +1,17 @@
+package com.aca.homework.week3.classroom;
+
+public class Student {
+    String name;
+    String surname;
+}
+
+
+/*
+Create a Student class that has name and surname fields.
+Create an AcaClass class with an array of students, startMillis, endMillis, name and fee fields.
+
+Create an object of AcaClass with name of "java level 1", which started on January 1 at 00:00, which ends on May 1
+ at 00:00 and has fee of 500.000, and has students with real student names and surnames.
+
+p.s. startMillis and endMillis must be the long type
+*/
