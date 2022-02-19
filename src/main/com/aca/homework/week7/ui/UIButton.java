1b844b28188b9d66db9f4e002330c5e7e3ec3b60
sargisEg <sargis.eg@gmail.com>
2022-02-20 01:16:05 +0400

    The solution of the 'ui view'

---
+++ src/main/com/aca/homework/week7/ui/UIButton.java
@@ -0,0 +1,21 @@
+package com.aca.homework.week7.ui;
+
+public class UIButton extends UIView {
+
+    private String buttonColor;
+
+    private UIButton(double width, double height, String buttonColor) {
+        super(width, height);
+        this.buttonColor = buttonColor;
+    }
+
+    public static UIButton createButton(double width, double height, String buttonColor) {
+        return new UIButton(width, height, buttonColor);
+    }
+
+    @Override
+    public void click() {
+        System.out.print("clicked on a button having a color " + buttonColor);
+        super.click();
+    }
+}
