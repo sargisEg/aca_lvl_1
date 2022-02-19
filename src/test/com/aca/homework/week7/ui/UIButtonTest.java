d293983a46861f406bfb1483a4cb77cf9eb60f5b
Sargis Eghoyan <98351848+sargisEg@users.noreply.github.com>
2022-02-20 01:37:12 +0400

    Delete src/test directory

--- src/test/com/aca/homework/week7/ui/UIButtonTest.java
+++
@@ -1,8 +0,0 @@
-package com.aca.homework.week7.ui;
-
-class UIButtonTest {
-    public static void main(String[] args) {
-        UIView button = UIButton.createButton(5, 5, "green");
-        button.click();
-    }
-}
