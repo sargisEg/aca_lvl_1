401816762398824e0d825852c0c79cf9e807a71e
sargisEg <sargis.eg@gmail.com>
2022-02-23 17:20:28 +0400

    Comment fixes in the solution of the 'button click' problem

--- src/main/com/aca/homework/week7/button/click/Button.java
+++ src/main/com/aca/homework/week7/button/click/Button.java
@@ -2,8 +2,13 @@ package com.aca.homework.week7.button.click;
 
 public class Button {
     private String text;
+    private Action action;
 
-    public void click(Action action) {
+    public Button(Action action) {
+        this.action = action;
+    }
+
+    public void click() {
         action.doAction();
     }
 }
--- src/test/com/aca/homework/week7/button/click/ButtonTest.java
+++ src/test/com/aca/homework/week7/button/click/ButtonTest.java
@@ -2,12 +2,12 @@ package com.aca.homework.week7.button.click;
 
 public class ButtonTest {
     public static void main(String[] args) {
-        Button button = new Button();
-        button.click(new Action() {
+        Button button = new Button(new Action() {
             @Override
             public void doAction() {
                 System.out.println("clicked on the button");
             }
         });
+        button.click();
     }
 }
