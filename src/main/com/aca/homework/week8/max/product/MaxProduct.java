a6a1ff52a1b19345aeaa61643b5fc4ac10b2942c
sargisEg <sargis.eg@gmail.com>
2022-03-05 22:28:30 +0400

    Error fixes in the solution of the 'max product' problem

--- src/main/com/aca/homework/week8/max/product/MaxProduct.java
+++ src/main/com/aca/homework/week8/max/product/MaxProduct.java
@@ -3,18 +3,18 @@ package com.aca.homework.week8.max.product;
 import java.math.BigDecimal;
 
 public class MaxProduct {
-    public static String calculateMaxProduct(int[] array) {
+    public static int calculateMaxProduct(int[] array) {
 
         if(array == null || array.length == 0)
             throw new IllegalArgumentException();
 
         BigDecimal product = BigDecimal.valueOf(1);
         int maxNegativeNumber = 1;
-        int notNullNumbersCount = 0;
+        int notZeroNumbersCount = 0;
 
         for (int i = 0; i < array.length; i++) {
             if (array[i] != 0) {
-                notNullNumbersCount++;
+                notZeroNumbersCount++;
                 product = product.multiply(BigDecimal.valueOf(array[i]));
             }
             if (array[i] < 0) {
@@ -23,18 +23,18 @@ public class MaxProduct {
             }
         }
 
-        if (array.length == 1 && array[0] < 0)
-            return "0";
+        if (array.length == 1)
+            return array[0];
 
-        if (notNullNumbersCount == 0)
-            return "0";
+        if (notZeroNumbersCount == 0)
+            return 0;
 
-        if (notNullNumbersCount == 1 && product.compareTo(BigDecimal.valueOf(0)) < 0)
-            return "0";
+        if (notZeroNumbersCount == 1 && product.compareTo(BigDecimal.valueOf(0)) < 0)
+            return 0;
 
         if (product.compareTo(BigDecimal.valueOf(0)) < 0)
             product = product.divide(BigDecimal.valueOf(maxNegativeNumber));
 
-        return product.toString();
+        return product.intValue();
     }
 }
