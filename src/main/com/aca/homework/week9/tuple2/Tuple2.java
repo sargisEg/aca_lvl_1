478197813822a264f14b769881ea77ce0131faa3
sargisEg <sargis.eg@gmail.com>
2022-03-05 22:38:26 +0400

    The solution of the 'tuple2' problem

---
+++ src/main/com/aca/homework/week9/tuple2/Tuple2.java
@@ -0,0 +1,20 @@
+package com.aca.homework.week9.tuple2;
+
+public class Tuple2<T1, T2> {
+
+    T1 t1;
+    T2 t2;
+
+    public Tuple2(T1 first, T2 second) {
+        this.t1 = first;
+        this.t2 = second;
+    }
+
+    public T1 getT1() {
+        return t1;
+    }
+
+    public T2 getT2() {
+        return t2;
+    }
+}
