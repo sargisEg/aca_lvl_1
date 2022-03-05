50773b6410c8b5ea083e1d8a07f4549dab765d3e
sargisEg <sargis.eg@gmail.com>
2022-03-05 20:52:41 +0400

    Error fixes in the solution of the 'clickable' problem

--- src/main/com/aca/homework/week7/clickable/Image.java
+++ src/main/com/aca/homework/week7/clickable/Image.java
@@ -1,9 +1,6 @@
 package com.aca.homework.week7.clickable;
 
 public class Image extends AbstractClickable {
-
-    private String name;
-
     public Image(String name, Action action) {
         super(name, action);
     }
