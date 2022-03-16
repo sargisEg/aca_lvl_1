60a98c3523a5fbc1600d24f181b999c61b072cb0
sargisEg <sargis.eg@gmail.com>
2022-03-16 23:29:04 +0400

    Added type ServletRequest for ObjectFactory interface in ServletRequestObjectFactory implementation

--- src/main/com/aca/homework/week10/object/factory/ServletRequestObjectFactory.java
+++ src/main/com/aca/homework/week10/object/factory/ServletRequestObjectFactory.java
@@ -1,6 +1,6 @@
 package com.aca.homework.week10.object.factory;
 
-public class ServletRequestObjectFactory implements ObjectFactory {
+public class ServletRequestObjectFactory implements ObjectFactory<ServletRequest> {
 
     @Override
     public ServletRequest getObject() {
