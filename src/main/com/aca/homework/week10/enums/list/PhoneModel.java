a12c3869c9d8230b43609fc2b06d94aef46366b2
sargisEg <sargis.eg@gmail.com>
2022-03-19 17:28:44 +0400

    Made getCameraDescriptionList method concrete

--- src/main/com/aca/homework/week10/enums/list/PhoneModel.java
+++ src/main/com/aca/homework/week10/enums/list/PhoneModel.java
@@ -5,24 +5,9 @@ import java.util.List;
 
 public enum PhoneModel {
 
-    NOKIA_1100 (Collections.emptyList()) {
-        @Override
-        public List<String> getCameraDescriptionList() {
-            return getCameraDescription();
-        }
-    },
-    NOKIA_3110 (Collections.singletonList("back camera")){
-        @Override
-        public List<String> getCameraDescriptionList() {
-            return getCameraDescription();
-        }
-    },
-    GALAXY_S2 (List.of("back camera", "front camera")){
-        @Override
-        public List<String> getCameraDescriptionList() {
-            return getCameraDescription();
-        }
-    };
+    NOKIA_1100 (Collections.emptyList()),
+    NOKIA_3110 (Collections.singletonList("back camera")),
+    GALAXY_S2 (List.of("back camera", "front camera"));
 
     private List<String> cameraDescription;
 
@@ -30,9 +15,7 @@ public enum PhoneModel {
         this.cameraDescription = cameraDescription;
     }
 
-    public List<String> getCameraDescription() {
+    public List<String> getCameraDescriptionList() {
         return cameraDescription;
     }
-
-    public abstract List<String> getCameraDescriptionList();
 }
