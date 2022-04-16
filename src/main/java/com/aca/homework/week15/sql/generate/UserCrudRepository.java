1cf64dd3bbeb23289448cf3f222a4cb01ceed6c2
sargisEg <sargis.eg@gmail.com>
2022-04-16 22:48:13 +0400

    Removed hard-wire of the sqlGenerator

--- src/main/java/com/aca/homework/week15/sql/generate/UserCrudRepository.java
+++ src/main/java/com/aca/homework/week15/sql/generate/UserCrudRepository.java
@@ -7,15 +7,17 @@ import java.sql.SQLException;
 public class UserCrudRepository {
 
     private final Connection connection;
+    private final SQLGenerator sqlGenerator;
 
     public UserCrudRepository(Connection connection) {
         this.connection = connection;
+        this.sqlGenerator = new SQLGenerator();
     }
 
     public User save(User user) {
 
         try {
-            connection.createStatement().execute(new SQLGenerator().generateInsert(user));
+            connection.createStatement().execute(sqlGenerator.generateInsert(user));
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
